082a1c0ad6dbfd4e0beccb7aca76ed79c026b7fb
DESKTOP-6UFLOMS\SEUNGGYU <happymin1026@gmail.com>
2022-12-21 14:44:09 +0900

    get function (in AWS Lambda)

---
+++ Lambda/get/lambda_function.py
@@ -0,0 +1,34 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+
+def lambda_handler(event, context):
+    # DynamoDB conference table list return
+    # Get the service resource.
+    dynamodb = boto3.resource('dynamodb')
+    
+    # Get the dynamoDB table
+    table = dynamodb.Table('conference')
+    
+    user_id = event.get('user_id', None)
+    type = event.get('type', None)
+    
+    if user_id='*':
+        table.scan()
+    else:
+        if type:
+            q = Key('user_id').eq(user_id) & Key('type').eq(type)
+        else:
+            q = Key('user_id').eq(user_id)
+            
+        response = table.query(
+            KeyConditionExpression=q
+        )
+        items = response['Items']
+
+    return {
+        'statusCode': 200,
+        'items': items,
+        'event': event
+    }
