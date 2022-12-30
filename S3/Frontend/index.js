f06697771fb01cc600b189e8fd1b0433c5745bd8
DESKTOP-6UFLOMS\SEUNGGYU <happymin1026@gmail.com>
2022-12-30 14:57:44 +0900

    Update frontend file

--- S3/Frontend/index.js
+++ S3/Frontend/index.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
-    var ENDPOINT = 'https://sz5wb0khgi.execute-api.ap-northeast-3.amazonaws.com/dev/msa-restaurant'
-    var CF = 'https://d2afiyjh228nwr.cloudfront.net'
+    var ENDPOINT = 'https://02x7yv1yv5.execute-api.ap-northeast-3.amazonaws.com/dev/reserve'
+    var CF = 'https://d2ltyemy31vr0.cloudfront.net'
     var dialog = document.querySelector('dialog');
     var showModalButton = $('.show-modal');
     if (! dialog.showModal) {
         dialogPolyfill.registerDialog(dialog);
     }
+    $("#start_date").on('change',function(){
+        $('#end_date').attr("min",$("#start_date").val());
+    })
+    $("#end_date").on('change',function(){
+        $('#start_date').attr("max",$("#end_date").val());
+    })
     $(document).on('click', '.show-modal', function(e) {
         e.preventDefault();
         var user = $(this).data('user');
-        var phone_number = $(this).data('phone_number');
-        $('#showBox').html('<img style="width:100%" src="'+CF+'/qrcodes/'+user+'/'+phone_number+'/qrcode.jpg"/>');
+        var type = $(this).data('type');
+        $('#showBox').html('<img style="width:100%" src="'+CF+'/qrcodes/'+user+'/'+type+'/qrcode.jpg"/>');
         dialog.showModal();
     });
     dialog.querySelector('.close').addEventListener('click', function() {
@@ -30,7 +36,7 @@ $(document).ready(function(){
                     html += '<li class="mdl-list__item mdl-list__item--three-line"> <span class="mdl-list__item-primary-content"> <i class="material-icons mdl-list__item-avatar">person</i> <span>'
                     html += item['user_name']
                     html += '</span> <span class="mdl-list__item-text-body">'
-                    //html += 'From. ' + item['company_name'] +'<br/> '+item['type']
+                    html += item['start_date'] +'~'+item['end_date']+'<br/> '+item['type']
                     html += '</span> </span> <span class="mdl-list__item-secondary-content"> <a data-user="'+item['user_id']+'" data-type="'+item['type']+'" class="show-modal mdl-list__item-secondary-action" href="#"><i class="material-icons">print</i></a> </span> </li>'
                 })
                 html += '</ul>'
@@ -45,18 +51,24 @@ $(document).ready(function(){
         });
     }
     $('#submitButton').on('click', function(e){
+        var user_id = $('#user_id').val();
+        var type = $('#type').val();
         var user_name = $('#user_name').val();
-        var user_phone = $('#user_phone').val();
-        var people = $('#num_people').val();
+        var user_phone = $('#phone_number').val();
+        var start_date = $('#start_date').val();
+        var end_date = $('#end_date').val();
         $.ajax({
             url: ENDPOINT,
             method: 'post',
             datatype: 'json',
             async: true,
             data:JSON.stringify({
+                type: type,
+                user_id: user_id,
                 user_name: user_name,
                 phone_number: user_phone,
-                num_people: people
+                start_date: start_date,
+                end_date :end_date
             }),
             beforeSend: function(){
                 $('#p2').show();
